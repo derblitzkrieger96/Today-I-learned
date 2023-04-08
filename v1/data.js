8a6079c5312740468999884ba76b2341c9597693
Dairon Danilo Dorado Castillo <ddanilodorado@gmail.com>
2023-04-07 22:45:16 -0500

    algo

--- v1/data.js
+++ v1/data.js
@@ -43,8 +43,6 @@ const initialFacts = [
   },
 ];
 
-const passWordDataBase = "OZyM2yncznRbsPT9";
-
 // LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
 
 // 👍 🤯 ⛔️
